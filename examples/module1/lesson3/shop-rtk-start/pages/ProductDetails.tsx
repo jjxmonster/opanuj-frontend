224f043ae9ea454ad13d2088c2c8b90b8abe8c8b
jjxmonster <kuba.tarabasz@wp.pl>
2024-06-20 23:08:31 +0200

    [M1L3] Replace context with rtk in header and product page

--- examples/module1/lesson3/shop-rtk-start/pages/ProductDetails.tsx
+++ examples/module1/lesson3/shop-rtk-start/pages/ProductDetails.tsx
@@ -1,14 +1,14 @@
-import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
-import { CartContext } from '../contexts/CartContext';
-import { ProductContext } from '../contexts/ProductContext';
+import { addToCart } from '../state/cartSlice';
+import { useAppDispatch } from '../hooks/rtk';
+import { useGetProductsQuery } from '../services/products';
 
 const ProductDetails = () => {
   const { id } = useParams();
-  const { addToCart } = useContext(CartContext);
-  const { products } = useContext(ProductContext);
+  const dispatch = useAppDispatch();
+  const { data } = useGetProductsQuery();
 
-  const product = products.find((item) => {
+  const product = data!.find((item) => {
     return item.id === parseInt(id!);
   });
 
@@ -37,7 +37,7 @@ const ProductDetails = () => {
             </div>
             <p className="mb-8">{description}</p>
             <button
-              onClick={() => addToCart(product)}
+              onClick={() => dispatch(addToCart(product))}
               className="bg-green-600 py-4 px-8 text-white"
             >
               Add to cart
